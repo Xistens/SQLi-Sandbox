00b9ad87df139ed9fc8024d15cb86dc64415f74a
xistens <cssparta@gmail.com>
2020-12-07 22:19:47 +0100

    Added slides to challenge 7

--- sqli_platform/challenges/challenge7/views.py
+++ sqli_platform/challenges/challenge7/views.py
@@ -43,7 +43,9 @@ def sessions():
         csession=session.get(f"{_bp}_user_id", None),
         csession_name=session.get(f"{_bp}_username", None),
         ctitle=get_config(f"{_bp}", "title"),
-        query=format_query(_query)
+        query=format_query(_query),
+        slides=f"{_bp}/slides/slides.html",
+        cdesc=get_config(f"{_bp}", "description")
     )
     _query = []
     return d
@@ -194,6 +196,8 @@ def book():
         book = {}
 
         query = f"SELECT id FROM books WHERE title like '{title}%'"
+        # query = f"SELECT id FROM books WHERE title like ?"
+        # params = [f"'{title}%'"]
         _query.append(query)
         bid = db.sql_query(_bp, query, one=True)
         if bid:
