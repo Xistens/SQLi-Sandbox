7984c056b5a4f15c7c40885ef6d5560a9b3a9ce5
xistens <cssparta@gmail.com>
2020-12-22 18:43:27 +0100

    Added getFlags API if debug enabled

--- sqli_platform/__init__.py
+++ sqli_platform/__init__.py
@@ -18,31 +18,38 @@ _challenge_config = 'config.json'
 
 
 app = Flask(__name__)
-app.debug = False
+app.debug = True
 
 # Change me
 app.secret_key = b"dummy_key"
-app.config["flag_key"] = "xV6WxVghQ7fFxarqnyj5EYwPmrGZgt4Q"
+app.config.update(
+    SESSION_COOKIE_SECURE=False,
+    SESSION_COOKIE_HTTPONLY=True,
+    SESSION_COOKIE_SAMESITE="Lax",
 
-# Enable/disable debug and guidance for the entire app
-app.config["enable_debug_query"] = True
-app.config["enable_guidance"] = True
+    JSONIFY_PRETTYPRINT_REGULAR = True,
+    # Enable/disable debug and guidance for the entire app
+    enable_debug_query = True,
+    enable_guidance = True,
+    
+    # Default value for debug and guidance setting (enabled/disabled)
+    debug_query_default = True,
+    guidance_default = False,
 
-# Default value for debug and guidance setting (enabled/disabled)
-app.config["debug_query_default"] = True
-app.config["guidance_default"] = False
+    # Enable/disable download page for extra material
+    enable_download = True,
 
-# Enable/disable download page for extra material
-app.config["enable_download"] = True
+    flag_key = "xV6WxVghQ7fFxarqnyj5EYwPmrGZgt4Q",
+    # "{FLAG}" will be replaced with the MD5 sum of challenge title and flag_key
+    flag_format = "THM{{FLAG}}",
 
+    LOG_FOLDER = f'{_basedir}/logs',
 
-# "{FLAG}" will be replaced with the MD5 sum of challenge title and flag_key
-app.config["flag_format"] = "THM{{FLAG}}"
+)
+
 
 limiter = Limiter(app, default_limits=[], key_func=get_remote_address)
 
-# Temp logging directory, remember to change?
-app.config['LOG_FOLDER'] = f'{_basedir}/logs'
 
 # Configure logging
 logger.init_logs(app)
@@ -53,14 +60,17 @@ app_log.debug("Initializing loggers")
 # Load configs from all challenges into memory
 _configs = utils.get_challenge_configs(_challenges_path, _challenge_config)
 
-if __name__ == "__main__":
-    from sqli_platform import database
 
-    db = database.Database(app, _configs, _basedir)
+# Register the API if in debug mode
+if app.debug:
+    from sqli_platform.api import api
+    app.register_blueprint(api)
 
-    # Import challenges modules and register their blueprints
-    utils.load_blueprints(app, limiter, _configs)
+from sqli_platform import database
+db = database.Database(app, _configs, _basedir)
 
+# Import challenges modules and register their blueprints
+utils.load_blueprints(app, limiter, _configs)
 
-    from sqli_platform import routes
+from sqli_platform import routes
 
