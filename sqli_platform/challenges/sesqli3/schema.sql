214f4e3c6cc25552ec14d5230a557287eca5f244
xistens <cssparta@gmail.com>
2021-01-09 10:02:23 +0100

    Changed password for flag account on sesqli login bypass challenges

--- sqli_platform/challenges/sesqli3/schema.sql
+++ sqli_platform/challenges/sesqli3/schema.sql
@@ -13,7 +13,7 @@ CREATE TABLE `usertable` (
 
 
 INSERT INTO `usertable` (`UID`, `name`, `profileID`, `salary`, `passportNr`, `email`, `nickName`, `password`) VALUES
-    (1, 'Francois', '10', 250, '8605255014084', '', '', 'ce5ca673d13b36118d54a7cf13aeb0ca012383bf771e713421b4d1fd841f539a'),
+    (1, 'Francois', '10', 250, '8605255014084', '', '', 'ccf4c101985e3bea45e7141af6be89308dbcc10f5763deb97b7d246c2dd9411c'),
     (2, 'Michandre', '11', 300, '9104154800081', '', '', '05842ffb6dc90bef3543dd85ee50dd302f3d1f163de1a76eee073ee97d851937'),
     (3, 'Colette', '12', 275, '8403024800086', '', '', 'c69d171e761fe56711e908515def631856c665dc234a0aa404b32c73bdbc81ac'),
     (4, 'Phillip', '13', 400, '8702245800084', '', '', 'b6efdfb0e20a34908c092725db15ae0c3666b3cea558fa74e0667bd91a10a0d3'),
